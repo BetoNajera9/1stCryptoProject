34b7347d9e2ab8e15c1ca80c8fba27fff412e774
Alfredo2117 <47376878+Alfredo2117@users.noreply.github.com>
2022-04-22 12:30:47 -0500

    Update rsa-oaep.py

--- rsa-oaep.py
+++ rsa-oaep.py
@@ -1,7 +1,8 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from base64 import b64decode,b64encode
 import time
+import binascii
+
 
 def rsaOAEP(message):
 	i=0
@@ -15,19 +16,22 @@ def rsaOAEP(message):
 			key = RSA.importKey(public_key)
 			cipher = PKCS1_OAEP.new(key)
 			ciphertext = cipher.encrypt(message)
-			print('Encriptacion:' )
-			print(b64encode(ciphertext))
-
 		elif i==1:
 			key = RSA.importKey(private_key)
 			cipher = PKCS1_OAEP.new(key)
 			message = cipher.decrypt(ciphertext)
+		end=time.time()
+		#Solamente los separo para que no se tome en cuenta la operacion binascci, las impresiones, etc.
+		if i ==0 :
+			print('Encriptacion:' )
+			print(binascii.hexlify(ciphertext))
+		elif i==1:
 			print('Decriptacion:' )
 			print(message)
-		end=time.time()
 		print('Tiempo de Ejecucion:' + str(end-start)) 
 		print('\n')
 		i=i+1
 
+	
 
 
