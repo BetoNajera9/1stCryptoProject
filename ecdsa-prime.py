626219b00d31fac584fe351a475d78b66d0d44e8
Alfredo2117 <47376878+Alfredo2117@users.noreply.github.com>
2022-04-22 12:33:13 -0500

    Update ecdsa-prime.py

--- ecdsa-prime.py
+++ ecdsa-prime.py
@@ -1,30 +1,29 @@
-
 from ecdsa import SigningKey, NIST521p
 import time
 import binascii
 
-#def ecdsaP(message):
-message=b'hola que hace'
-sk = SigningKey.generate(curve=NIST521p)
-vk = sk.verifying_key
-i=0
-while i<2:
-	start= time.time()
-	if i==0:
-		signature = sk.sign(message)
-	elif i==1:
-		validator= vk.verify(signature, message)
-	end=time.time()
-	#Solamente los separo para que no se tome en cuenta la operacion binascci, las impresiones, etc.
-	if i==0:
-		print('Firma Obtenida:.')
-		print(binascii.hexlify(signature))
-	elif i==1:
-		print('Validacion Realizada.')
-		print(validator)
-	print('Tiempo de Ejecucion:' + str(end-start)) 
-	print('\n')
-	i=i+1
+def ecdsaP(message):
+	#message=b'hola que hace'
+	sk = SigningKey.generate(curve=NIST521p)
+	vk = sk.verifying_key
+	i=0
+	while i<2:
+		start= time.time()
+		if i==0:
+			signature = sk.sign(message)
+		elif i==1:
+			validator= vk.verify(signature, message)
+		end=time.time()
+		#Solamente los separo para que no se tome en cuenta la operacion binascci, las impresiones, etc.
+		if i==0:
+			print('Firma Obtenida:.')
+			print(binascii.hexlify(signature))
+		elif i==1:
+			print('Validacion Realizada.')
+			print(validator)
+		print('Tiempo de Ejecucion:' + str(end-start)) 
+		print('\n')
+		i=i+1
 
 
 
