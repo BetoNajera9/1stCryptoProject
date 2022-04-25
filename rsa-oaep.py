26f9c49491a9ec9b37135e9c422bfc22f88daa2d
Alfredo2117 <47376878+Alfredo2117@users.noreply.github.com>
2022-04-24 22:55:39 -0500

    Update rsa-oaep.py

--- rsa-oaep.py
+++ rsa-oaep.py
@@ -1,37 +1,45 @@
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-import time
+from Crypto import Random
+import timeit
 import binascii
 
 
-def rsaOAEP(message):
-	i=0
+
+
+def rsaPSS(message):
+	print('=======================================================================')
+	print('Test RSA-PSS')
+	#message=b'hola que hace'
 	key = RSA.generate(2048)
 	public_key = key.publickey().export_key()
 	private_key = key.export_key()
+	i=0
 
 	while i < 2 :
-		start= time.time()
+		h = SHA256.new(message)
+		start= timeit.default_timer()
 		if i ==0 :
-			key = RSA.importKey(public_key)
-			cipher = PKCS1_OAEP.new(key)
-			ciphertext = cipher.encrypt(message)
+			key = RSA.import_key(private_key)
+			signature = pss.new(key).sign(h)
 		elif i==1:
-			key = RSA.importKey(private_key)
-			cipher = PKCS1_OAEP.new(key)
-			message = cipher.decrypt(ciphertext)
-		end=time.time()
+			key = RSA.import_key(public_key)
+			verifier = pss.new(key)
+			verifier.verify(h,signature)
+		end=timeit.default_timer()
 		#Solamente los separo para que no se tome en cuenta la operacion binascci, las impresiones, etc.
 		if i ==0 :
-			print('Encriptacion:' )
-			print(binascii.hexlify(ciphertext))
+			print('Firma Generada:' )
+			print(binascii.hexlify(signature))
 		elif i==1:
-			print('Decriptacion:' )
-			print(message)
+			print('Verificacion Realizada.')	
+	  
+		
 		print('Tiempo de Ejecucion:' + str(end-start)) 
-		print('\n')
 		i=i+1
 
-	
+
+
 
 
