debbed2ca4dc2ff0fdb25e57c7a11c151f631424
Alfredo2117 <47376878+Alfredo2117@users.noreply.github.com>
2022-04-25 07:26:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import subprocess
 import random, string
+import csv
 
 def run(*popenargs, **kwargs):
     input = kwargs.pop("input", None)
@@ -59,22 +60,35 @@ def main():
   print('Se realizan 10 preubas a la vez')
   lengthString=input('De una longitud para el tamano de vector que se quiera probar: ')
   lengthString=int(lengthString)
-  numberStrings=10
+  numberStrings=50
   n=1
 
   for x in range(numberStrings):
     strs.append(createString(lengthString))
 
+  data1=[]
+  data2=[]
+  data=[]
+  header=['CHACHA-20 Cifrado', 'CHACHA-20 Decifrado', 'AES-EBC Cifrado', 'AES-EBC Decifrado', 'AES-GCM Cifrado', 'AES-GCM Decifrado','RSA-OAEP Cifrado' ,'RSA-OAEP Decifrado','SHA 2 (384)',' SHA 2 (512)',' SHA 3 (384)','SHA 3 (512)','ECDSA P Firma','ECDSA P Verificacion' , 'ECDSA B Firma','ECDSA B Verificacion', 'RSA- PSS Firma','RSA- PSS Verificacion' ]
   for i in strs:
     print('Prueba ' + str(n))
     print('String de prueba' + str(bytes(i, 'utf-8')))
-    chachaes.algoritmos(bytes(i, 'utf-8'))
-    sha.main(bytes(i, 'utf-8'))
-    rsa_oae.rsaOAEP(bytes(i, 'utf-8'))
-    rsa_pss.rsaPSS(bytes(i, 'utf-8'))
-    ecdsa_prime.ecdsaP(bytes(i, 'utf-8'))
-    ecdsa_binary.ecdsaB(bytes(i, 'utf-8'))
+    data1=chachaes.algoritmos(bytes(i, 'utf-8'))
+    data3=rsa_oae.rsaOAEP(bytes(i, 'utf-8'))
+    data2=sha.main(bytes(i, 'utf-8'))
+    data4=rsa_pss.rsaPSS(bytes(i, 'utf-8'))
+    data5=ecdsa_prime.ecdsaP(bytes(i, 'utf-8'))
+    data6=ecdsa_binary.ecdsaB(bytes(i, 'utf-8'))
     print('\n') 
+    data=data1+data2+data3+data4+data5+data6
+    with open('vector.csv', 'a', newline='', encoding='UTF8') as f:
+      writer = csv.writer(f)
+      if n==1:
+        # write the header
+        writer.writerow(header)
+
+      # write the data
+      writer.writerow(data)
     n=n+1
 
   # except Exception as e:
