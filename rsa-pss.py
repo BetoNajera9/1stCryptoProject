cfa0e739f5fbc8c292c1efd6717af4b4fc6ce429
Alfredo2117 <47376878+Alfredo2117@users.noreply.github.com>
2022-04-24 22:57:00 -0500

    Update rsa-pss.py

--- rsa-pss.py
+++ rsa-pss.py
@@ -2,22 +2,24 @@ from Crypto.Signature import pss
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto import Random
-import time
+import timeit
 import binascii
 
 
 
 
 def rsaPSS(message):
+	print('=======================================================================')
+	print('Test RSA-PSS')
+	#message=b'hola que hace'
 	key = RSA.generate(2048)
 	public_key = key.publickey().export_key()
 	private_key = key.export_key()
 	i=0
-	print('RSA-PSS -----------------------------------------------------------------------------')
 
 	while i < 2 :
 		h = SHA256.new(message)
-		start= time.time()
+		start= timeit.default_timer()
 		if i ==0 :
 			key = RSA.import_key(private_key)
 			signature = pss.new(key).sign(h)
@@ -25,13 +27,20 @@ def rsaPSS(message):
 			key = RSA.import_key(public_key)
 			verifier = pss.new(key)
 			verifier.verify(h,signature)
-		end=time.time()
+		end=timeit.default_timer()
 		#Solamente los separo para que no se tome en cuenta la operacion binascci, las impresiones, etc.
 		if i ==0 :
 			print('Firma Generada:' )
 			print(binascii.hexlify(signature))
 		elif i==1:
 			print('Verificacion Realizada.')	
+	  
+		
+		print('Tiempo de Ejecucion:' + str(end-start)) 
+		i=i+1
+
+
+	
 	  
 		
 		print('Tiempo de Ejecucion:' + str(end-start)) 
